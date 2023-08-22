e3ba9543ae70991b817d94c1fae82768f04c504c
Robert Matwiejczyk <robmatwiejczyk@gmail.com>
2023-08-22 13:07:02 +0200

    Image poster fixes.

--- src/scripts/masonry.js
+++ src/scripts/masonry.js
@@ -1,11 +1,3 @@
-// var $grid = document.querySelector(".grid");
-// imagesLoaded($grid, function () {
-//   new Masonry(".grid", {
-//     percentPosition: true,
-//     gutter: 15,
-//   });
-// });
-
 new Masonry(".tiles", {
   percentPosition: true,
   gutter: 15,
