85aebb6a9a1b07040962887e9b82bcb7f05c080b
Robert Matwiejczyk <robmatwiejczyk@gmail.com>
2023-08-22 10:25:28 +0200

    Enable masonry again.

--- src/scripts/masonry.js
+++ src/scripts/masonry.js
@@ -1,7 +1,12 @@
-var $grid = document.querySelector(".grid");
-imagesLoaded($grid, function () {
-  new Masonry(".grid", {
-    percentPosition: true,
-    gutter: 15,
-  });
+// var $grid = document.querySelector(".grid");
+// imagesLoaded($grid, function () {
+//   new Masonry(".grid", {
+//     percentPosition: true,
+//     gutter: 15,
+//   });
+// });
+
+new Masonry(".tiles", {
+  percentPosition: true,
+  gutter: 15,
 });
