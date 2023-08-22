b439402c9c2fe6852a4c7004ab9710d80d5673d8
Robert Matwiejczyk <robmatwiejczyk@gmail.com>
2023-08-22 10:16:11 +0200

    Blurred placeholder images.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroImageTools],
   site: "https://eurobob.github.io",
   base: "/jimena",
 });
